e542de2f876b0455b8e0ab9699c9626fe2d3dace
Katrina Pritchard <pritckat@gmail.com>
2018-11-05 17:55:56 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20180915193511) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password_digest"
+    t.string "password"
   end
 
 end
